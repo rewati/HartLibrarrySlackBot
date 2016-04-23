5735633cdb2c835f942b09c64a65d50c309ef4c0
rewati <rewati.raman@gmail.com>
2016-04-23 03:17:14 -0700

    test

--- src/main/java/com/rr/hartlibrarryslackbot/InMemoryDb.java
+++ src/main/java/com/rr/hartlibrarryslackbot/InMemoryDb.java
@@ -1,19 +1,15 @@
 package com.rr.hartlibrarryslackbot;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Rewati Raman(rewati.raman@gmail.com).
  */
 
-@Configuration
 public class InMemoryDb {
 
-    @Value("db.file.loc")
     private String dbFileLocation;
     private static InMemoryDb inMemoryDb;
     private InMemoryDb(){
-        
+
     }
 }
