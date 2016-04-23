e9bdbb4d609eb540c4e940e8de45d6235e109827
rewati <rewati.raman@gmail.com>
2016-04-23 03:59:52 -0700

    test

--- src/main/java/com/rr/hartlibrarryslackbot/Controller.java
+++ src/main/java/com/rr/hartlibrarryslackbot/Controller.java
@@ -1,14 +1,15 @@
 package com.rr.hartlibrarryslackbot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Rewati Raman(rewati.raman@gmail.com).
@@ -26,10 +27,23 @@ public class Controller {
 
     @RequestMapping("/isbn")
     String checkDbForBook(@RequestBody String msg) {
-        return msg;
+        Map<String, String> msgMap = splitToMap(msg);
+        String returnMsg = msgMap.get("user_name")+" searched for ISBN "+msgMap.get("text")+" .\n";
+        returnMsg = returnMsg + " This book is available at Hart Library.";
+        return returnMsg;
     }
 
     public static void main(String arg[]) {
         SpringApplication.run(Controller.class,arg);
+    }
+
+    private Map<String, String> splitToMap(String str) {
+        String[] tokens = str.split("\n");
+        Map<String, String> map = new HashMap<String, String>();
+        for (int i=0; i<tokens.length-1; ++i ) {
+            String[] keyValue = tokens[i].split("=");
+            map.put(keyValue[0],keyValue[1]);
+        }
+        return map;
     }
 }
