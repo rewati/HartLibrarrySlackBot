55351d2b63c21a15d3fc8188e5df2f2112d0b2ab
rewati <rewati.raman@gmail.com>
2016-04-23 03:12:34 -0700

    Initial commit

---
+++ src/main/java/com/rr/hartlibrarryslackbot/Controller.java
@@ -0,0 +1,29 @@
+package com.rr.hartlibrarryslackbot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Rewati Raman(rewati.raman@gmail.com).
+ */
+@ComponentScan
+@RestController
+@EnableAutoConfiguration
+public class Controller {
+
+    @RequestMapping("/isbn/{isbn}")
+    String home(@PathVariable String isbn){
+
+        return "hello world";
+    }
+
+    public static void main(String arg[]) {
+        SpringApplication.run(Controller.class,arg);
+    }
+}
